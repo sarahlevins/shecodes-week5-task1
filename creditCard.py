565c7f43975475ecd4218c71f00cbc7154fceaee
sarahlevins <sarahjeanlevins@gmail.com>
2019-08-22 10:09:06 -0700

    completed credit card

--- creditCard.py
+++ creditCard.py
@@ -1,32 +1,44 @@
-    #turn string input into a list of integers
 def calculate_credit_card_number_check_digit(ccnum):   
     credit_list = []
     for digit in range(len(ccnum)):
         credit_list.append (int(ccnum[digit]))
-    print (credit_list)
 
     times_one_list = credit_list[-2::-2]
-    print (sum(times_one_list))
 
     times_two_list = [i * 2 for i in credit_list[-1::-2]]
-    print(times_two_list)
 
-    boop = 0
+    times_two_count = 0
     for number in times_two_list:
         if number >= 10:
-            print (boop)
-            print (number)
-            number_one = number // 10
-            number_two = number - number_one * 10
-            times_two_list [boop] = number_one + number_two
-            print(times_two_list[boop])
-            boop += 1
+            times_two_list [times_two_count] = number - 9
+            times_two_count += 1
         else:
-            boop += 1
-    print (times_two_list)
+            times_two_count += 1
 
+    return str((((sum(times_two_list) + sum(times_one_list)) * 9) % 10))
 
-calculate_credit_card_number_check_digit('542418027979176')
+def validate_credit_card_number_check_digit(ccnum):
+    credit_list = []
+    for digit in range(len(ccnum)):
+        credit_list.append (int(ccnum[digit]))
+
+    times_one_list = credit_list[-1::-2]
+    times_two_list = [i * 2 for i in credit_list[-2::-2]]
+
+    times_two_count = 0
+    for number in times_two_list:
+        if number >= 10:
+            times_two_list [times_two_count] = number - 9
+            times_two_count += 1
+        else:
+            times_two_count += 1
+    if (sum(times_one_list) + sum (times_two_list)) % 10 == 0:
+        return ('This is a valid credit card number.')
+    else:
+        return ('This is an invalid credit card number.')
+
+# calculate_credit_card_number_check_digit('542418027979176')
+# validate_credit_card_number_check_digit('5424180279791762')
 
 
 
